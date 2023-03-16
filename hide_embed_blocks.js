6758aff54fc787f4ba5d7b818c9caff49ce36c51
Bret Farley <34036924+Mr-RoBret@users.noreply.github.com>
2023-03-16 09:14:36 -0500

    places embedArr in main function, fixes embed part

--- hide_embed_blocks.js
+++ hide_embed_blocks.js
@@ -9,33 +9,35 @@ wp.domReady(function () {
     xhr.send();
     // xhr.responseType = "json";
     xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
 
-        const data = xhr.response;
-        const data_parsed = JSON.parse(data);
+            const data = xhr.response;
+            const data_parsed = JSON.parse(data);
 
-        const embeds_list = data_parsed['variation_array_text'];
+            const embeds_list = data_parsed['variation_array_text'];
 
-        allowed_embeds_array = embeds_list.split(', ');
-        allowed_embeds_array.forEach( (item) => {
-            return item.trim();
-        })
-        console.log(allowed_embeds_array);
-        // console.log(JSON.parse(embed_array));
+            allowed_embeds_array = embeds_list.split(', ');
+            allowed_embeds_array.forEach( (item) => {
+                return item.trim();
+            })
+            console.log(allowed_embeds_array);
+            // console.log(JSON.parse(embed_array));
 
-    } else {
-        console.log(`Error: ${xhr.status}`);
-    }
-};
-
-    // for each item in block variations matching type 'core/embed'...
-    const embedArr = wp.blocks.getBlockVariations('core/embed');
-    console.log(embedArr);
-    embedArr.forEach(function (blockVariation) {
-        //  if item doesn't exist in allowed blocks array, unregister from blocks 
-        if (-1 === allowed_embeds_array.indexOf(blockVariation.name)) {
-            wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name);
+        } else {
+            console.log(`Error: ${xhr.status}`);
         }
-    });
+        
+        // get list of block variations for type "embed"
+        const embedArr = wp.blocks.getBlockVariations('core/embed');
+        
+        // for each item in embed variations... 
+        embedArr.forEach(function (blockVariation) {
+            //  if item doesn't exist in allowed blocks array, unregister from blocks 
+            if (-1 === allowed_embeds_array.indexOf(blockVariation.name)) {
+                wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name);
+            }
+        });
+        
+    };
 });
 
