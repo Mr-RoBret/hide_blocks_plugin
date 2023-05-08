ccf555d8e9ecedcd2296ed8340cc75926e3881ae
Bret Farley <34036924+Mr-RoBret@users.noreply.github.com>
2023-05-08 09:17:30 -0500

    cleans up old files and removes examples folder

--- includes/hide-blocks-scripts.php
+++ includes/hide-blocks-scripts.php
@@ -1,31 +1,5 @@
 <?php 
 
-// function plugin_scripts() {
-    
-//     wp_enqueue_script(
-//         'hide_variation_blocks',
-//         HIDEBLOCKS_URL . 'js/hide_variation_blocks.js',
-//         // array( 'jquery', 'wp-blocks', 'wp-dom', 'wp-block-editor', 'wp-element', 'wp-i18n', 'wp-edit-post' ),
-//         array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-site' ),
-//         '1.0.0',
-//         true
-//     );
-// }
-
-// function stolaf_deny_variation_blocks(){
-
-//     wp_enqueue_script(
-//         'hide_variation_blocks',
-//         HIDEBLOCKS_URL . 'js/hide_variation_blocks.js',
-//         array( 'jquery', 'wp-blocks', 'wp-dom', 'wp-block-editor', 'wp-element', 'wp-i18n', 'wp-edit-post' ),
-//         '1.0.0',
-//         true
-//     );
-// }
-
-// add_action( 'admin_enqueue_scripts', 'plugin_scripts' );
-// add_action( 'enqueue_block_editor_assets', 'plugin_scripts' );
-
 function plugin_scripts() {
     
     $dependencies = array( 'wp-blocks', 'wp-dom-ready' );
