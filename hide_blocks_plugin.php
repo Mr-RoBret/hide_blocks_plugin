11469514037d8c222ac050d01b1384707226d9c2
Bret Farley <34036924+Mr-RoBret@users.noreply.github.com>
2023-05-10 13:46:08 -0500

    updates version number

--- hide_blocks_plugin.php
+++ hide_blocks_plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Hide Blocks Plugin
 Description: Populates list of blocks with toggles for turning on & off in block selector
-Version: 0.1.2
+Version: 0.1.3
 Contributors: bfarley01
 Author: Bret Farley
 License: GPLv2 or later
