09b2d3fb0cad137da73be27079351fc405dea843
Bret Farley <34036924+Mr-RoBret@users.noreply.github.com>
2023-05-11 11:25:56 -0500

    adds 2nd arg of unregister and formats correctly

--- includes/hide-blocks-scripts.php
+++ includes/hide-blocks-scripts.php
@@ -1,32 +1,23 @@
 <?php 
 
-function plugin_scripts() {
-    
-    $dependencies = array( 'wp-blocks', 'wp-dom-ready' );
-    
-    if( !function_exists( 'get_current_screen' ) ) {
-        require_once(ABSPATH . 'wp-admin/includes/screen.php');
-    } 
+// function plugin_scripts() {
+    function my_enqueue_script() {
+        global $pagenow;
+        
+        if ( is_admin() && $pagenow === 'post.php' ) {
+            write_to_console($pagenow);
 
-        if( get_current_screen()->id == 'page' ) {
-            // var_dump($current_screen);
-            array_push($dependencies, 'page');
-        } elseif( get_current_screen()->id == 'widgets' ) {
-            array_push($dependencies, 'wp-edit-widgets');
-        } else {
-            array_push($dependencies, 'wp-edit-post');
+            wp_enqueue_script( 
+                'hide_variation_blocks', 
+                HIDEBLOCKS_URL . 'js/hide_variation_blocks.js', __FILE__ , 
+                array( 'wp-blocks' ), '1.0', true );
+                wp_script_add_data( 'my-script', 'groups', array( 'wp-blocks' ) );
+                wp_script_add_data( 'my-script', 'position', 10 
+            );
         }
-    
-    wp_enqueue_script(
-        'hide_variation_blocks', 
-        HIDEBLOCKS_URL . 'js/hide_variation_blocks.js', __FILE__ , 
-        $dependencies, 
-        1, 
-        false 
-    );
-}
+    }
 
-add_action( 'enqueue_block_editor_assets', 'plugin_scripts' );
+    add_action( 'admin_enqueue_scripts', 'my_enqueue_script' );
 
 
 function inserter_scripts() {
@@ -42,6 +33,28 @@ function inserter_scripts() {
         );
     }
 
-add_action( 'enqueue_block_editor_assets', 'inserter_scripts' )
+add_action( 'enqueue_block_editor_assets', 'inserter_scripts' );
+
+
+/**
+ * Simple helper to debug to the console
+ *
+ * @param $data object, array, string $data
+ * @param $context string  Optional a description.
+ *
+ * @return string
+ */
+function write_to_console($data, $context = 'Debug in Console') {
+
+    // Buffering to solve problems frameworks, like header() in this and not a solid return.
+    ob_start();
+
+    $output  = 'console.info(\'' . $context . ':\');';
+    $output .= 'console.log(' . json_encode($data) . ');';
+    $output  = sprintf('<script>%s</script>', $output);
+
+    echo $output;
+
+}
 
 ?>
